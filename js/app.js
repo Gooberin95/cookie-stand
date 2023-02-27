d0907d5a1b4f797286d29fdd47e71a0a15e91a95
Brandon Ruiz <Sober4202020@outlook.com>
2023-02-27 05:25:55 +0000

    this is a stuggle

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 
 'use strict';
 let sectionElem = document.getElementById('seattle');
+let sectionElem6 = document.getElementById('salmon');
 console.log(sectionElem);
 
-let sectionElem6 = document.getElementById('salmon');
+
 let trash = [];
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
@@ -14,54 +15,42 @@ function randomAge(min, max) {
 // console.log(seattle.sum);
 
 
-function Store(minCust, maxCust, averageCookieSale, name, sum, customers, hrSales) {
+function Store(minCust, maxCust, averageCookieSale, name) {
   this.name = name;
-  this.sum = sum;
-  this.customers = customers;
-  this.hrSales = hrSales;
+  this.sum = 0;
+  this.customersPerHour = [];
+  this.hrSales = [];
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.averageCookieSale = averageCookieSale;
-
-
-
-  trash.push(this);
+  
 }
 
-Store.prototype.getSum = function (){}
+
+Store.prototype.SalesHour = function () {
+
     for(let i = 0; i < hours.length; i++) {
-  this.customers.push(randomAge(this.minCust, this.maxCust));
+      let customers = randomAge(this.minCust, this.maxCust);
+
+      let tally = this.customersPerHour.push(randomAge(this.minCust, this.maxCust));
+      this.hrSales.push(tally);
+      this.sum += tally;
+
+
 }
 
-store.prototype.hrSum = function () {
+
+}
+
+Store.prototype.Summing = function () {
   this.getSum();
   for (let i = 0; i < this.customers.length; i++) {
     let cookies = this.customers[i] * this.avgCookieSale;
     this.hrSales.push(Math.floor(cookies));
     this.sum += Math.floor(cookies);
   }
-
+  
 }
-Store.prototype.render = function () {
-    this.hrSum();
-    let articleElem = document.createElement('article');
-    sectionElem.appendChild(articleElem);
-    let hrElem = document.createElement('h1');
-    hrElem.textContent = `${this.name}`;
-    articleElem.appendChild(hrElem);
-    let ulElem = document.createElement('ul');
-    articleElem.appendChild(ulElem);
-    for (let i = 0; i < hours.length; i++) {
-      let liElem = document.createElement('li');
-      liElem.textContent = `${hours[i]} ${this.hrSales[i]} cookies`;
-      ulElem.appendChild(liElem);
-    }
-
-    let liTotal = document.createElement('li');
-    liTotal.textContent = `Total cookies:${this.sum}`;
-    ulElem.appendChild(liTotal);
-
-  },
 
 
 Store.prototype.rendering = function () {
@@ -80,7 +69,7 @@ Store.prototype.rendering = function () {
   th2Elem.textContent = "Daily total";
   let th3Elem = document.createElement('th');
   row1.appendChild(th3Elem);
-  th3Elem.textContent = this.name;
+  th3Elem.textContent = this.sum;
 
   // second row
   let row2 = document.createElement('tr');
@@ -98,11 +87,15 @@ Store.prototype.rendering = function () {
 
 
 
-let homeTown = new Store(23, 65, 6.3, 'Seattle', 0, [], []);
-homeTown.hrSum();
+let homeTown = new Store(23, 65, 6.3, 'Seattle');
+console.log(homeTown);
+console.log(homeTown.SalesHour());
 
 
-let tokyo = new Store(3,24,1.2, 'Tokyo',)
-homeTown.
-homeTown.rendering();
+
+
+
+// let tokyo = new Store(3,24,1.2, 'Tokyo',)
+// homeTown.
+// homeTown.rendering();
 console.log(trash);
